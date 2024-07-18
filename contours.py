19417a40cbcd185dd9bcff09fbaae31a5cab7daf
amiskus999 <ampofojohnson08a@gmail.com>
2024-07-18 12:47:41 -0400

    On branch main

--- contours.py
+++ contours.py
@@ -1,15 +1,15 @@
 import cv2
 from picamera2 import Picamera2
-import numpy as np 
+import numpy as np
 import time
 
-   
+
 #do not touch this, needed for 64bit
 #Picamera2 library handles the camera
-picam2 = Picamera2() 
+picam2 = Picamera2()
 picam2.preview_configuration.main.size = (920,720)
 picam2.preview_configuration.main.format = "RGB888"
-picam2.preview_configuration.controls.FrameRate = 60
+picam2.preview_configuration.controls.FrameRate = 120
 picam2.preview_configuration.align()
 picam2.configure("preview")
 picam2.start()
@@ -25,11 +25,11 @@ vHigh = 1
 def trackHue(val):
     global hLow
     hLow = val
-    
+
 def trackSat(val):
     global sLow
     sLow = val
-    
+
 def trackVal(val):
     global vLow
     vLow = val
@@ -60,7 +60,7 @@ while True:
     im = picam2.capture_array() #grab a frame
     #### To Do ....... #########
     cv2.putText(im,"fps: "+str(int(fps)),pos,cv2.FONT_HERSHEY_SIMPLEX,1.5,(0,0,255),3)
-    
+
     #cv2.circle(im,(440,350),100,rColor,2)
     lowBound = np.array([hLow,sLow,vLow])
     highBound = np.array([hHigh,sHigh,vHigh])
@@ -78,14 +78,14 @@ while True:
         x,y,w,h = cv2.boundingRect(contour)
         cv2.rectangle(im,(x,y),(x+w,y+h),(0,0,255),3)
     #print(hLow ," ",hHigh)
-    #### END 
+    #### END
     cv2.imshow("Camera", im)
     cv2.imshow("mask",smask)
     cv2.imshow("OBJ",obj)
     t2 = time.time()
     diff = t2-t1
     fps = 0.9*fps + 0.1*(1/diff)
-    
+
     key = cv2.waitKey(1) #wait for user input
     if key==ord('q'):
         break
@@ -96,4 +96,3 @@ while True:
 picam2.stop()
 picam2.close()
 cv2.destroyAllWindows()
-
